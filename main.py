89d464d8a2a1fcd64c53a72b8d60b2a24885b0e5
Nydelic-Arch <nydelic89006@gmail.com>
2026-01-11 22:34:37 +0800

    wip: manga scrawler pipeline working

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print("hello")
-wwww = 0
+from side import quick_get
+from side import has_chinese
+
+url = "https://yhdm.one/"
+soup = quick_get(url)
+
+lbls = soup.find("div", class_="col-md-8").find_all("section")
+
+print("=" * 50)
+for lbl in lbls:
+    try:
+        print(lbl.find(class_="title pb-0").text)
+        animes = lbl.find_all("li", class_="col-3 mb-3")
+        for anime in animes:
+            try:
+                year = anime.find("span").text
+                if has_chinese(year):
+                    year = "連載中"
+                title = anime.find("div", class_="small text-truncate").text
+                link = "https://yhdm.one" + anime.find("a")["href"]
+
+                print(year, title, "  ", link)
+            except:
+                continue
+        print("=" * 50)
+    except:
+        continue
--- side.py
+++ side.py
@@ -1,1 +1,14 @@
-print("hello")
+import requests
+from bs4 import BeautifulSoup
+
+
+def quick_get(url) -> BeautifulSoup:
+    rq = requests.get(url)
+    return BeautifulSoup(rq.text, "lxml")
+
+
+import re
+
+
+def has_chinese(text: str) -> bool:
+    return bool(re.search(r"[\u4e00-\u9fff]", text))
